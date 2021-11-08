10f61fc0df3201a42758761debc7541acf2a158a
Maksim Titiaiev <gipsmas@gmail.com>
2021-11-08 21:53:13 +0200

    fix: handle errors for file permitions

--- my_ciphering_cli/helpers.js
+++ my_ciphering_cli/helpers.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const { FileNotExist, PermitionsError } = require('./errors.js')
 
 /**
  * 
@@ -9,10 +10,14 @@ const path = require('path')
  function tryCreateReadStreamForPath(filePath) {
     if (typeof filePath === 'undefined') return null
     try {
+        if (!fs.statSync(path.resolve(filePath)).isFile()) throw new PermitionsError(filePath)
         fs.accessSync(path.resolve(filePath), fs.constants.R_OK)
         return fs.createReadStream(path.resolve(filePath), { encoding: 'utf-8' })
     } catch (error) {
+        if (error.code === 'ENOENT') throw new FileNotExist(filePath)
+        if (error.code === 'EACCES') throw new PermitionsError(filePath)
         throw error
+        
     }
 }
 
@@ -24,9 +29,12 @@ const path = require('path')
 function tryCreateWriteStreamForPath(filePath) {
     if (typeof filePath === 'undefined') return null
     try {
+        if (!fs.statSync(path.resolve(filePath)).isFile()) throw new PermitionsError(filePath)
         fs.accessSync(path.resolve(filePath), fs.constants.W_OK)
         return fs.createWriteStream(path.resolve(filePath), { encoding: 'utf-8', flags: 'r+', start: fs.statSync(path.resolve(filePath)).size })
     } catch (error) {
+        if (error.code === 'ENOENT') throw new FileNotExist(filePath)
+        if (error.code === 'EACCES') throw new PermitionsError(filePath)
         throw error
     }
 }
