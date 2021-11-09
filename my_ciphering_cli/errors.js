1040c0ae90518a244ee41f157694418becabdb3f
Maksim Titiaiev <gipsmas@gmail.com>
2021-11-09 12:03:51 +0200

    refactor: apply eslint

--- my_ciphering_cli/errors.js
+++ my_ciphering_cli/errors.js
@@ -1,51 +1,51 @@
 class ErrorWithCode extends Error {
-    constructor(code, message) {
-        super(message)
-        this.code = code
-    }
+  constructor (code, message) {
+    super(message)
+    this.code = code
+  }
 }
 
 class InvalidOption extends ErrorWithCode {
-    constructor(optName) {
-        super(1, `Недопустимая опция: "${optName}"`)
-    }
+  constructor (optName) {
+    super(1, `Недопустимая опция: "${optName}"`)
+  }
 }
 
 class OptionDuplication extends ErrorWithCode {
-    constructor(optName) {
-        super(5, `Опция "${optName}" продублирована несколько раз`)
-    }
+  constructor (optName) {
+    super(5, `Опция "${optName}" продублирована несколько раз`)
+  }
 }
 
 class MissingConfigOption extends ErrorWithCode {
-    constructor() {
-        super(6, `Отсутствует обязательная опция "--config"`)
-    }
+  constructor () {
+    super(6, 'Отсутствует обязательная опция "--config"')
+  }
 }
 
 class InvalidConfig extends ErrorWithCode {
-    constructor(message = 'Невалидная конфигурация') {
-        super(7, `${message}\nПример правильной конфигурации "C1-A-R0"`)
-    }
+  constructor (message = 'Невалидная конфигурация') {
+    super(7, `${message}\nПример правильной конфигурации "C1-A-R0"`)
+  }
 }
 
 class FileNotExist extends ErrorWithCode {
-    constructor(file = '') {
-        super(8, `"${file}" файл не обнаружен`)
-    }
+  constructor (file = '') {
+    super(8, `"${file}" файл не обнаружен`)
+  }
 }
 
 class PermitionsError extends ErrorWithCode {
-    constructor(file = '') {
-        super(9, `Нет прав на доступ к файлу или "${file}" не является файлом`)
-    }
+  constructor (file = '') {
+    super(9, `Нет прав на доступ к файлу или "${file}" не является файлом`)
+  }
 }
 
 module.exports = {
-    InvalidOption,
-    OptionDuplication,
-    MissingConfigOption,
-    InvalidConfig,
-    FileNotExist,
-    PermitionsError
+  InvalidOption,
+  OptionDuplication,
+  MissingConfigOption,
+  InvalidConfig,
+  FileNotExist,
+  PermitionsError
 }
--- my_ciphering_cli/index.js
+++ my_ciphering_cli/index.js
@@ -3,31 +3,28 @@ const { parseArgs, parseConfigString } = require('./parsers.js')
 const { tryCreateReadStreamForPath, tryCreateWriteStreamForPath } = require('./helpers.js')
 const { JobManager } = require('./job-manager.js')
 
-
 main(process.argv.slice(2))
 
-function main(args = []) {
+function main (args = []) {
+  try {
+    const { configString, inputFile, outputFile } = parseArgs(args)
 
-    try {
-        const { configString, inputFile, outputFile } = parseArgs(args)
+    const settings = {
+      /** @type {Array<string>} */
+      config: parseConfigString(configString),
 
-        const settings = {
-            /** @type {Array<string>} */
-            config: parseConfigString(configString),
+      /**  @type {NodeJS.ReadStream} */
+      input: inputFile ? tryCreateReadStreamForPath(inputFile) : process.stdin,
 
-            /**  @type {NodeJS.ReadStream} */
-            input: inputFile ? tryCreateReadStreamForPath(inputFile) : process.stdin,
-
-            /** @type {NodeJS.WriteStream} */
-            output: outputFile ? tryCreateWriteStreamForPath(outputFile) : process.stdout
-        }
-
-        new JobManager()
-            .createPiplineFromCofig(settings.config)
-            .run(settings.input, settings.output)
-
-    } catch (err) {
-        process.stderr.write(`ERROR: ${err.message}\nexitCode: ${err.code}\n`)
-        process.exit(err.code || 1)
+      /** @type {NodeJS.WriteStream} */
+      output: outputFile ? tryCreateWriteStreamForPath(outputFile) : process.stdout
     }
+
+    new JobManager()
+      .createPiplineFromCofig(settings.config)
+      .run(settings.input, settings.output)
+  } catch (err) {
+    process.stderr.write(`ERROR: ${err.message}\nexitCode: ${err.code}\n`)
+    process.exit(err.code || 1)
+  }
 }
